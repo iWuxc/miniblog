0682cc9837c567ba5209c4f5c3ad956f38ccd6b6
wuxioachen <wuxc.eng@gmail.com>
2025-08-16 10:45:09 +0800

    feat: 添加配置项功能

---
+++ cmd/mb-apiserver/app/server.go
@@ -0,0 +1,83 @@
+package app
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/iWuxc/miniblog/cmd/mb-apiserver/app/options"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var configFile string //配置文件路径
+
+func NewMiniBlogCommand() *cobra.Command {
+	// 常见默认的应用命令行选项
+	opts := options.NewServerOptions()
+	cmd := &cobra.Command{
+		// 指定命令的名字，该名字会出现在帮助信息中
+		Use: "mb-apiserver",
+		// 命令的简短描述
+		Short: "A mini blog show best practices for develop a full-featured Go project",
+		// 命令的详细描述
+		Long: `A mini blog show best practices for develop a full-featured Go project.
+
+The project features include:
+• Utilization of a clean architecture;
+• Use of many commonly used Go packages: gorm, casbin, govalidator, jwt, gin, 
+  cobra, viper, pflag, zap, pprof, grpc, protobuf, grpc-gateway, etc.;
+• A standardized directory structure following the project-layout convention;
+• Authentication (JWT) and authorization features (casbin);
+• Independently designed log and error packages;
+• Management of the project using a high-quality Makefile;
+• Static code analysis;
+• Includes unit tests, performance tests, fuzz tests, and mock tests;
+• Rich web functionalities (tracing, graceful shutdown, middleware, CORS, 
+  recovery from panics, etc.);
+• Implementation of HTTP, HTTPS, and gRPC servers;
+• Implementation of JSON and Protobuf data exchange formats;
+• The project adheres to numerous development standards: 
+  code standards, versioning standards, API standards, logging standards, 
+  error handling standards, commit standards, etc.;
+• Access to MySQL with programming implementation;
+• Implemented business functionalities: user management and blog management;
+• RESTful API design standards;
+• OpenAPI 3.0/Swagger 2.0 API documentation;
+• High-quality code.`,
+		// 命令出错时，不打印帮助信息。设置为 true 可以确保命令出错时一眼就能看到错误信息
+		SilenceUsage: true,
+		// 指定调用 cmd.Execute() 时，执行的 Run 函数
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// 将viper中的配置解析到选项opts变量中
+			if err := viper.Unmarshal(opts); err != nil {
+				return err
+			}
+
+			// 对命令行选项值进行校验
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+
+			fmt.Printf("ServerMode from ServerOptions: %s\n", opts.JWTKey)
+			fmt.Printf("ServerMode from Viper: %s\n\n", viper.GetString("jwt-key"))
+
+			jsonData, _ := json.MarshalIndent(opts, "", "  ")
+			fmt.Println(string(jsonData))
+
+			return nil
+		},
+		// 设置命令运行时的参数检查，不需要指定命令行参数。例如：./miniblog param1 param2
+		Args: cobra.NoArgs,
+	}
+
+	//初始化配置函数，在每个命令行运行的时候调用
+	cobra.OnInitialize(onInitialize)
+
+	// cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	// 推荐使用配置文件来配置应用，便于管理配置项
+	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", filePath(), "Path to the miniblog configuration file.")
+
+	// 将 ServerOptions 中的选项绑定到命令标志
+	opts.AddFlags(cmd.PersistentFlags())
+
+	return cmd
+}
--- cmd/mb-apiserver/main.go
+++ cmd/mb-apiserver/main.go
@@ -6,8 +6,21 @@
 
 package main
 
-import "fmt"
+import (
+	"github.com/iWuxc/miniblog/cmd/mb-apiserver/app"
+	_ "go.uber.org/automaxprocs"
+	"os"
+)
 
+// Go 程序的默认入口函数。阅读项目代码的入口函数.
 func main() {
-	fmt.Println("Hello, world222!")
+	// 创建迷你博客命令
+	command := app.NewMiniBlogCommand()
+
+	//执行命令并处理错误
+	if err := command.Execute(); err != nil {
+		// 如果发生错误，则退出程序
+		// 返回退出码，可以使其他程序（例如 bash 脚本）根据退出码来判断服务运行状态
+		os.Exit(1)
+	}
 }
