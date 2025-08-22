f461a5ced304af3d7b245c807664119a033daed3
wuxioachen <wuxc.eng@gmail.com>
2025-08-22 20:18:21 +0800

    feat: 实现http客户端的拦截中间件

--- internal/apiserver/httpserver.go
+++ internal/apiserver/httpserver.go
@@ -10,7 +10,8 @@ import (
 	"context"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
-	handler "github.com/iWuxc/miniblog/internal/apiserver/handler/grpc"
+	handler "github.com/iWuxc/miniblog/internal/apiserver/handler/http"
+	mw "github.com/iWuxc/miniblog/internal/pkg/middleware/gin"
 	"github.com/iWuxc/miniblog/internal/pkg/server"
 	"net/http"
 )
@@ -28,6 +29,9 @@ func (c *ServerConfig) NewGinServer() server.Server {
 	//创建Gin引擎
 	engine := gin.New()
 
+	// 注册全局中间件，用于恢复 panic、设置 HTTP 头、添加请求 ID 等
+	engine.Use(gin.Recovery(), mw.NoCache, mw.Cors, mw.Secure, mw.RequestIDMiddleware())
+
 	//注册REST API路由
 	c.InstallRESTAPI(engine)
 
@@ -42,10 +46,10 @@ func (c *ServerConfig) InstallRESTAPI(engine *gin.Engine) {
 	InstallGenericAPI(engine)
 
 	//创建核心业务处理器
-	handler.NewHandler()
+	handler := handler.NewHandler()
 
 	//注册健康检查接口
-	//engine.GET("/healthz", handler.Healthz)
+	engine.GET("/healthz", handler.Healthz)
 }
 
 // InstallGenericAPI 注册业务无关的路由，例如 pprof、404 处理等.
@@ -61,7 +65,7 @@ func InstallGenericAPI(engine *gin.Engine) {
 
 // 注册 API 路由。路由的路径和 HTTP 方法，严格遵循 REST 规范.// RunOrDie 启动 Gin 服务器，出错则程序崩溃退出.
 func (s *ginServer) RunOrDie() {
-	select {}
+	s.srv.RunOrDie()
 }
 
 // GracefulStop 优雅停止服务器.
